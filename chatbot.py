19568db3cf67c91b88c644d78435c13a8ea825a1
Camden McKay <cam.mckay9@gmail.com>
2024-11-15 18:14:22 -0600

    converted chatbot to fill the blank

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,7 @@ genai.configure(api_key="") #Input API key from https://aistudio.google.com/app/
 model = genai.GenerativeModel("gemini-1.5-flash") #Choose a model
 chat = model.start_chat(
     history=[
-        {"role": "user", "parts": "Your name is Michael now. Michael is a fisherman. Michael likes to go on walks and can't stop talking about the outdoors."} #Specifies what the bot should be like
+        {"role": "user", "parts": "You have a bubbly personality and love watching soccer."} #Specifies what the bot should be like
     ]
 )
 
@@ -12,11 +12,11 @@ class colors: #Used in coloring the terminal output
     YELLOW = '\033[33m'
     ENDC = '\033[m'
 
-introduction = chat.send_message("Introduce yourself.") #Causes AI to generate initial prompt
-print(colors.YELLOW + introduction.text + colors.ENDC) #Print initial prompt
-user_input = input("Prompt: ") #Ask user for input the first time
+ #Causes AI to generate initial prompt
+ #Print initial prompt
+ #Ask user for input the first time
 
-while user_input != "exit": #Check if the user typed exit to quit the program
-    response = chat.send_message(user_input) #Generate AI response to user input
-    print(colors.YELLOW + response.text + colors.ENDC) #Print AI response
-    user_input = input("Prompt: ") #Ask for user input any sequential time
+ #Check if the user typed exit to quit the program
+ #Generate AI response to user input
+ #Print AI response
+ #Ask for user input any sequential time
